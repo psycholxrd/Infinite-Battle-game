6afc514f29ee719f5fdc6d99a1c7131ddf969b8f
Marat Parashchenko <maratnyancat@gmail.com>
2026-01-08 07:21:39 +0100

    implemented tick logic

--- gameTicks.js
+++ gameTicks.js
@@ -1,6 +1,42 @@
+//60hz Monitors only for now
+const FRAMES_TO_SKIP = 10;
+
 class TimeController{
     constructor(){
-        this.skipFrames = 0;
+        this.skippedFrames = 0;
         this.paused = false;
     }
 }
+
+class GameClock{
+    constructor(timeController, callbackFn, args=[]){
+        this.ticksPassed = 0;
+        this.timeController = timeController;
+        this.callbackFn = callbackFn;
+        this.args = args;
+    }
+    onTick(){
+        if(!this.timeController.paused) window.requestAnimationFrame(()=>{this.onTick()});
+        if(this.timeController.skippedFrames === FRAMES_TO_SKIP){
+            this.callbackFn(...this.args);
+            this.timeController.skippedFrames = 0;
+            this.ticksPassed++;
+        }
+        this.timeController.skippedFrames++;
+    }
+    start(){
+        window.requestAnimationFrame(()=>{this.onTick()});
+    }
+    pause(){
+        this.timeController.paused = true;
+    }
+    resume(){
+        if(this.timeController.paused){
+            this.timeController.paused = false;
+            window.requestAnimationFrame(()=>{this.onTick()});
+        }
+    }
+}
+
+const tc = new TimeController();
+const gc = new GameClock(tc, () => {console.log("tick")});
